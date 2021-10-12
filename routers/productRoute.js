179f3da99dc70b8057878af6a952002a3ae82b56
dhruv <17bmiit075@gmail.com>
2021-10-12 17:32:32 +0530

    done

--- routers/productRoute.js
+++ routers/productRoute.js
@@ -5,7 +5,7 @@ const productController = require("../controllers/productController");
 router.post("/add", auth, productController.addProduct);
 router.get("/", productController.showProduct);
 router.get("/cat", productController.getProductByCategory);
-// router.get("/mostliked", productController.mostLiked);
+
 router.get("/recent", productController.recentProduct);
 router.patch("/modi/:id", auth, productController.modufyProduct);
 router.delete("/rem/:id", auth, productController.removeProducts);
